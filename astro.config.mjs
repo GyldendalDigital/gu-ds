e1af4b3e2e7a9c55a363d84019e9caab0b548688
Dagfinn <dagfinn.reitan@gyldendal.no>
2023-09-12 10:07:38 +0200

    Add Netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), svelte()]
+  integrations: [react(), vue(), svelte()],
+  output: "server",
+  adapter: netlify()
 });
