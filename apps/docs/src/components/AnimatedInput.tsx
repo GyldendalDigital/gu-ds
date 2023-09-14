ce83dc79f79e8b1233c7c8292660cfa659560b1c
Dagfinn <dagfinn.reitan@gyldendal.no>
2023-09-14 19:32:28 +0200

    Add azure table integration

---
+++ apps/docs/src/components/AnimatedInput.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, FunctionComponent } from "react";
+
+let knutDone = false;
+
+/** Half assed attempt to make a fancy input */
+export const AnimatedInput: FunctionComponent<{ initialValue: string }> = ({
+  initialValue,
+}) => {
+  const [placeHolder, setPlaceHolder] = useState<string | undefined>("");
+
+  const knut = "Knut?";
+  const gud = "GUDS?";
+
+  useEffect(() => {
+    const timeoutID = window.setTimeout(() => {
+      if (placeHolder == undefined) return;
+
+      if (knutDone) {
+        setPlaceHolder(gud.slice(0, placeHolder.length + 1));
+      } else {
+        setPlaceHolder(knut.slice(0, placeHolder.length + 1));
+      }
+      if (placeHolder === knut) {
+        knutDone = true;
+        setTimeout(() => {
+          setPlaceHolder("");
+        }, 2000);
+      }
+    }, 250);
+
+    return () => window.clearTimeout(timeoutID);
+  }, [placeHolder]);
+
+  return (
+    <input
+      type="text"
+      name="name"
+      defaultValue={initialValue}
+      placeholder={placeHolder}
+    />
+  );
+};
