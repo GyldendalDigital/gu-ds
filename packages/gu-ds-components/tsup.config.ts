11651032975e04ed9df3a034806968a6d1d79f08
Håvard Kvalvik <havard.kvalvik@gyldendal.no>
2023-09-19 13:23:37 +0200

    Fixed a problem with tsup entries

--- packages/gu-ds-components/tsup.config.ts
+++ packages/gu-ds-components/tsup.config.ts
@@ -1,22 +1,32 @@
+import fs from "fs";
 import { defineConfig } from "tsup";
 import tsconfig from "./tsconfig.json";
 
+// Generate entries in the following format:
+//
+// {
+//   'example/react': 'src/example/react.ts',
+//   'example/web-component': 'src/example/web-component.ts'
+// }
+
+const componentNames = fs.readdirSync("src");
+const entries: Record<string, string> = {};
+componentNames.forEach((componentName) => {
+  const directory = `src/${componentName}`;
+  const stats = fs.statSync(directory);
+  if (stats.isDirectory()) {
+    entries[`${componentName}/react`] = `${directory}/react.ts`;
+    entries[`${componentName}/web-component`] = `${directory}/web-component.ts`;
+  }
+});
+
 export default defineConfig((options) => ({
-  entry: [
-    "src/register-web-components.ts",
-    "src/**/react.ts",
-    "src/**/web-component.ts",
-  ],
+  entry: entries,
   dts: true,
   outDir: "dist",
   format: ["esm"],
   name: "gu-ds-components",
   splitting: false,
-  outExtension() {
-    return {
-      js: `.js`,
-    };
-  },
   sourcemap: true,
   clean: true,
   target: tsconfig.compilerOptions.target as "es2016",
