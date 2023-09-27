c62f970dd292fb73558d47a064ed15d190ce2a2b
Ola Imerslund <ola.imerslund@gyldendal.no>
2023-09-27 11:57:45 +0200

    Add addon-a11y to storybook

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -18,6 +18,7 @@ const config = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
